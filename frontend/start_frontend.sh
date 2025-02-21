810efba9c3cebb1b7b50abf0934a57edf63d82c5
Thomas Pesendorfer <thomas.pesendorfer@urbangames.com>
2025-02-21 22:27:45 +0100

    adding start-script for linux systems

---
+++ frontend/start_frontend.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate virtual environment if exists
+if [ -d "venv" ]; then
+    source venv/bin/activate
+fi
+
+# Start Uvicorn with the specified settings
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+
